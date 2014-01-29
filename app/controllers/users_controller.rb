efdfabcf7268373bd3172bacf8a704bb71d0c132
7 hills <yedukondala.reddy@cuelogic.co.in>
2014-01-29 17:45:39 +0530

    Resolving conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid? & @user.save
       redirect_to @user, :flash => { :notice => t(:user_created) }
     else
       render :action => :new, :flash => { :error => t(:user_not_created) }
